87f615f2960d2c5f323d401b4841eb3d8d356381
Jonathan Bello <jonathan.bellofg@gmail.com>
2022-05-21 21:35:59 -0500

    corrections of models types

--- pkg/blog/blog.go
+++ pkg/blog/blog.go
@@ -12,7 +12,7 @@ type Model struct {
 	Body            string         `gorm:"type:text;not null" json:"body"`
 	Description     string         `gorm:"type:varchar(100);not null" json:"description"`
 	Url             *string        `gorm:"type:varchar(256)" json:"url"`
-	Type            uint8          `gorm:"type:varchar(256);not null" json:"type"`
+	Type            uint8          `gorm:"not null" json:"type"`
 	Techs           []tech.Model   `gorm:"many2many:blog_techs" json:"techs"`
 	Authors         []author.Model `gorm:"many2many:blog_authors" json:"authors"`
 }
