c64cddba919eaca42cb83d45795fe6e06edfec9f
Jonathan Bello <jonathan.bellofg@gmail.com>
2022-05-21 21:29:50 -0500

    corrections of blogs model in body type

--- pkg/blog/blog.go
+++ pkg/blog/blog.go
@@ -9,10 +9,10 @@ import (
 type Model struct {
 	pkg.GlobalModel `gorm:"embedded"`
 	Title           string         `gorm:"type:varchar(100);not null" json:"title"`
-	Body            string         `gorm:"type;Text;not null" json:"body"`
+	Body            string         `gorm:"type:text;not null" json:"body"`
 	Description     string         `gorm:"type:varchar(100);not null" json:"description"`
 	Url             *string        `gorm:"type:varchar(256)" json:"url"`
-	Type            uint8          `gorm:"type:varchar(256)" json:"type"`
+	Type            uint8          `gorm:"type:varchar(256);not null" json:"type"`
 	Techs           []tech.Model   `gorm:"many2many:blog_techs" json:"techs"`
 	Authors         []author.Model `gorm:"many2many:blog_authors" json:"authors"`
 }
